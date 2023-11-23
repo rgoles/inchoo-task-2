9b5d142a0a10b994ce7f5b1d52dd4b1ca5dfa602
Renato Goleš <renato@Renatos-MacBook-Air.local>
2023-11-23 22:32:43 +0100

    dizajn dizajn

---
+++ inchoo-task-2/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+        listStyleType: {
+            none: 'none',
+            disc: 'disc',
+            decimal: 'decimal',
+            square: 'square',
+            roman: 'upper-roman',
+        },
+
+    },
+    plugins: [],
+}
+
